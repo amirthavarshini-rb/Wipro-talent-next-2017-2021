0e3d040d242d398e35749c64320e870f2b52f1dc
Amirthavarshini <64469576+amirthavarshini-rb@users.noreply.github.com>
2020-05-28 19:37:51 +0530

    Add files via upload

---
+++ Flow Control Statements/prog7.java
@@ -0,0 +1,10 @@
+public class prog7 { 
+public static void main(String[] args) { 
+	Scanner s=new Scanner(System.in); 
+	char ch=s.next().charAt(0);
+	if (Character.isLowerCase(ch)) {
+		System.out.println(ch+"->"+Character.toUpperCase(ch)); }
+	else {
+		System.out.println(ch+"->"+Character.toLowerCase(ch)); }
+       }
+}
---
+++ Flow Control Statements/prog_10.java
@@ -0,0 +1,7 @@
+public class prog10 { 
+public static void main(String[] args) { 
+	int i;
+	for(i=1;i<=10;i++){
+		System.out.print(i+"	");
+       }
+}
