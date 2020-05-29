5dbc1ca35b5762df342906b7d5b2e263ad2307cf
Amirthavarshini <64469576+amirthavarshini-rb@users.noreply.github.com>
2020-05-29 09:47:45 +0530

    Delete prog4.java

--- Arrays/prog4.java
+++
@@ -1,8 +0,0 @@
-public class prog4{
-    public static void main(String[] args) {
-        int arr[]={65,67,70,97,99};
-        for(int  i=0;i<arr.length;i++) {
-                System.out.println((char)arr[i]);
-        }
-    }
-}
