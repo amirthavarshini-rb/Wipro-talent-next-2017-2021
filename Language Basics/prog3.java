0ccac71b8826026a2930a8d913809bf640012440
Amirthavarshini <64469576+amirthavarshini-rb@users.noreply.github.com>
2020-05-29 09:37:37 +0530

    Delete prog3.java

--- Language Basics/prog3.java
+++
@@ -1,9 +0,0 @@
-public class prog3{  
-    public static void main(String[] args){ 
-	int a,b,s;
-	a=Integer.parseInt(args[0]);
-	b=Integer.parseInt(args[1]);
-	s=a+b;
-        	System.out.println("The sum of "+args[0]+" and "+args[1]+" is "+ s);
-    }  
-}  
