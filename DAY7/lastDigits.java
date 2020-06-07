32c5b9d1ad2a44c54884b7be39eee2c8c4cb7e7e
Amirthavarshini <64469576+amirthavarshini-rb@users.noreply.github.com>
2020-06-07 18:41:29 +0530

    Update lastDigits.java

--- DAY7/lastDigits.java
+++ DAY7/lastDigits.java
@@ -1,9 +1,9 @@
 import java.io.*;
 import  java.util.*;
 class UserMainCode{
-  public int addLastDigits(int input1){
+  public int addLastDigits(int input1,int input2){
   int n1 = Math.abs(input1 % 10);
-		int n2 = Math.abs(input2 % 10);
-		return n1 + n2;
+  int n2 = Math.abs(input2 % 10);
+  return n1 + n2;
 	}
 }
