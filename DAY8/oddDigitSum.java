752bf1d513f4e9e0be2fd78cdeca541afed00b2e
Amirthavarshini <64469576+amirthavarshini-rb@users.noreply.github.com>
2020-06-08 11:30:06 +0530

    Create oddDigitSum.java

---
+++ DAY8/oddDigitSum.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode{
+  public int oddDigitSum(int input1){
+  if(input1>=1 && input1<=25000){
+		int sum = 0;
+		while(input1 != 0){
+			int r = input1 % 10;
+			if(r % 2 != 0)
+				sum += r;
+			input1 /= 10;
+		}
+		return sum;
+	}
+	return 0;
+}
+}
