19a145942783fb385be448da24b796f4d66ae335
Amirthavarshini <64469576+amirthavarshini-rb@users.noreply.github.com>
2020-06-08 10:30:58 +0530

    Create nonRepeatDigits.java

---
+++ DAY8/nonRepeatDigits.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode{
+  public int nonRepeatDigitsCount(int input1){
+  if(input1>=1 && input1 <=25000){
+			int c=0;
+			int[] a = new int[10];
+			while(input1>0){
+				int r = input1 % 10;
+				a[r]++;
+				input1 /= 10;
+			}
+			for(int i=0;i<10;i++){
+				if(a[i]==1)
+					c++;
+			}
+			return c;
+		}
+		return 0;
+	}
+}
