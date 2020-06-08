f2df23a150ca7ddffe3ba44e7cde56e5c12355a6
Amirthavarshini <64469576+amirthavarshini-rb@users.noreply.github.com>
2020-06-08 10:11:45 +0530

    Create digitsCount.java

---
+++ DAY8/digitsCount.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode{
+  public int allDigitsCount(int input1){
+  int c=0,r;
+		while(input1!=0){
+			r = input1 % 10;
+			c++;
+			input1 /= 10;
+		}
+		return c;
+	}
+}
+
